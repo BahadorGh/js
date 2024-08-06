73809e6f2ab75e0d7ec6483c6046fffd5333d0c5
Kien Ngo <github@kien.ngo>
2024-08-06 19:51:42 -0400

    Update

--- apps/dashboard/src/contract-ui/tabs/tokens/components/mint-button.tsx
+++ apps/dashboard/src/contract-ui/tabs/tokens/components/mint-button.tsx
@@ -1,6 +1,5 @@
 import { MinterOnly } from "@3rdweb-sdk/react/components/roles/minter-only";
 import { Icon, useDisclosure } from "@chakra-ui/react";
-import type { useContract } from "@thirdweb-dev/react";
 import { thirdwebClient } from "lib/thirdweb-client";
 import { defineDashboardChain } from "lib/v5-adapter";
 import { FiPlus } from "react-icons/fi";
@@ -9,25 +8,21 @@ import { Button, Drawer } from "tw-components";
 import { TokenERC20MintForm } from "./mint-form-erc20";
 
 interface TokenMintButtonProps {
-  contractQuery: ReturnType<typeof useContract>;
+  contractAddress: string;
+  chainId: number;
 }
 
 export const TokenMintButton: React.FC<TokenMintButtonProps> = ({
-  contractQuery,
+  contractAddress,
+  chainId,
   ...restButtonProps
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const contractV4 = contractQuery.contract;
-  const contract = contractV4
-    ? getContract({
-        address: contractV4.getAddress(),
-        chain: defineDashboardChain(contractV4.chainId),
-        client: thirdwebClient,
-      })
-    : null;
-  if (!contract || !contractV4) {
-    return null;
-  }
+  const contract = getContract({
+    address: contractAddress,
+    chain: defineDashboardChain(chainId),
+    client: thirdwebClient,
+  });
   return (
     <MinterOnly contract={contract}>
       <Drawer
